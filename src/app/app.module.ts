cc63a8ab288d90a77776dfea31281eadeae12567
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-09-07 18:07:15 +0930

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+ 
 @NgModule({
   declarations: [
     MyApp,
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html',
+	animations: [
+		trigger('itemState', [
+			transition('void => *', [
+				style({transform: 'translateX(-100%)'}),
+				animate('500ms ease-out')
+			]),
+			transition('* => void', [
+				animate('500ms ease-in', style({transform: 'translateX(100%)'}))	
+			])
+		])
+	]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	items: string[] = [];
 
-  }
+	constructor(public navCtrl: NavController) {
+
+	}
+
+	addItem(){
+		this.items.push('test');
+	}
+
+	deleteItem(item){
+		this.items.splice(this.items.indexOf(item), 1);
+	}
 
 }
